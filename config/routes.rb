cf5f8a30469afc1231a358ceb2e1dab1e9fd2d02
Vicente Lucas <vica.escorcio@gmail.com>
2017-12-27 21:13:34 -0300

    cruds and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :accounts
   resources :agencies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'accounts#index'
 end
